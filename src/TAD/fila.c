841f7e0344fc856401a3972891715dbaa2391143
helenalves-dev <helenalves7384@gmail.com>
2026-05-18 21:56:16 -0300

    feat(fila): Fila was implemented.

---
+++ src/TAD/fila.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fila.h" 
+
+Fila * criarFila(){
+    Fila* fila = (Fila*) malloc(sizeof(Fila));
+    if (fila != NULL){
+        fila->inicio = NULL;
+        fila->fim = NULL;
+    }
+    return fila;
+};
+
+void enfileirarReserva( Fila * fila , Reserva reserva ){
+    NoFila* novo = (NoFila*) malloc(sizeof(NoFila));
+    if (novo == NULL){
+        printf("Erro ao alocar memória para reserva.\n");
+        return;
+    }
+    novo->reserva = reserva;
+    novo->prox = NULL;
+    if(fila->inicio == NULL){
+        fila->inicio = novo;
+        fila->fim = novo;
+    }else{
+        fila->fim->prox = novo;
+        fila->fim = novo;
+    }
+};
+
+Reserva desenfileirarReserva( Fila * fila ){
+    if(filaVazia(fila)){
+        printf("Fila vazia. Não há reservas para desenfilerar.\n");
+        Reserva vazia;
+        strcpy(vazia.nomeUsuario, "");
+        vazia.codigoLivro = -1;
+        return vazia;
+    }
+    NoFila* temp = fila->inicio;
+    fila->inicio = temp->prox;
+    if(fila->inicio == NULL){
+        fila->fim = NULL;
+    }
+    temp->prox = NULL;
+    return temp->reserva;
+};
+
+int filaVazia( Fila * fila ){
+    return (fila->inicio == NULL);
+};
+
+void exibirReservas( Fila * fila ){
+    if(filaVazia(fila)){
+        printf("Fila vazia. Não há reservas para exibir.\n");
+        return;
+    }
+    NoFila* temp = fila->inicio;
+    while (temp != NULL){
+        printf("Usuario: %s\nCodigo do Livro: %d\n\n", temp->reserva.nomeUsuario, temp->reserva.codigoLivro);
+        temp = temp->prox;
+    }
+};
---
+++ src/TAD/fila.h
@@ -0,0 +1,29 @@
+#ifndef FILA_H
+#define FILA_H
+
+typedef struct {
+    char nomeUsuario [100];
+    int codigoLivro ;
+} Reserva ;
+
+typedef struct NoFila {
+    Reserva reserva;
+    struct NoFila * prox;
+} NoFila ;
+
+typedef struct Fila {
+    NoFila * inicio ;
+    NoFila * fim ;
+} Fila ;
+
+Fila * criarFila();
+
+void enfileirarReserva( Fila * fila , Reserva reserva );
+
+Reserva desenfileirarReserva( Fila * fila );
+
+int filaVazia( Fila * fila );
+
+void exibirReservas( Fila * fila );
+
+#endif
