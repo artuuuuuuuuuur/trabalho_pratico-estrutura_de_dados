058eab6f9bc6afe77e85ef357fef565ef46bb95f
Artur de Maria <arturdemaria7@gmail.com>
2026-06-01 08:38:18 -0300

    feature/main-interface: added Arvore TAD params in main interface functions

--- src/interfaces/acoes.h
+++ src/interfaces/acoes.h
@@ -1,20 +1,21 @@
 #include<TAD/livro.h>
 #include<TAD/listaEncadeada.h>
 #include<TAD/fila.h>
+#include<TAD/arvore.h>
 
 #ifndef ACOES_H
 #define ACOES_H
 
-int cadastrarNovoLivro(Livro* livro /*TAD Árvore*/) {}
-int buscarLivroPorCodigo(Livro* livro /*TAD Árvore*/) {}
-int listarLivrosCrescente(/*TAD Árvore*/) {}
-int listarLivrosPre(/*TAD Árvore*/) {}
-int listarLivrosPos(/*TAD Árvore*/) {}
+int cadastrarNovoLivro(Livro* livro, Arvore* arvore) {}
+int buscarLivroPorCodigo(Livro* livro, Arvore* arvore) {}
+int listarLivrosCrescente(Arvore* arvore) {}
+int listarLivrosPre(Arvore* arvore) {}
+int listarLivrosPos(Arvore* arvore) {}
 int emprestimoLivro(Livro* livro, Lista* listaDeEmprestimos) {}
 int devolverLivro(Livro* Livro, Lista* listaDeEmprestimos) {}
 void exibirReservas(Fila* filaDeReservas) {}
-int exibirQuantidadeLivros(/*TAD Árvore*/) {}
-int exibirAlturaArvore(/*TAD Árvore*/) {}
+int exibirQuantidadeLivros(Arvore* arvore) {}
+int exibirAlturaArvore(Arvore* arvore) {}
 
 
 #endif ACOES_H
