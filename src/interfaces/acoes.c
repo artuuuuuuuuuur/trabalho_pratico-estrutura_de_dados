1585ba4acdd961b041d71381458183564433709a
Artur de Maria <arturdemaria7@gmail.com>
2026-06-01 08:49:00 -0300

    feature/main-interface: register new book function  was added to main interface

--- src/interfaces/acoes.c
+++ src/interfaces/acoes.c
@@ -3,6 +3,19 @@
 #include<stdlib.h>
 #include<string.h>
 
+int cadastrarNovoLivro(Livro* livro, Arvore* arvore) {
+    if (livro == NULL) {
+        printf("Erro: o livro não existe.\n");
+        return 0;
+    }
+    if (arvore == NULL) {
+        printf("Erro: a árvore não existe.\n");
+        return 0;    
+    }
+    inserirLivroArvore(arvore, livro);
+    return 1;
+}
+
 int emprestimoLivro(const Livro * livro, Lista* listaDeEmprestimos, const char * nomeUsuario) {
     if(livro == NULL) {
         printf("Erro: o livro não existe.\n");
